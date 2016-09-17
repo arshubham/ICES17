6c03f373e0f8874bd6e3975947f103c49ae2e8ba
Shubham Arora <shubham.arora4538@gmail.com>
2016-09-17 22:29:24 +0530

    some fixes

--- js/custom.js
+++ js/custom.js
@@ -8,6 +8,16 @@ $(window).load(function() {
 });
 
 
+$(document).ready(function() {
+ 
+    setTimeout(function(){
+        $('body').addClass('loaded');
+        $('h1').css('color','#222222');
+    }, 3000);
+ 
+});
+
+
 /* ----------------------------- 
 Backgroung slider
 ----------------------------- */
